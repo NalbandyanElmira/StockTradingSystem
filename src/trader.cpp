5b4772b798bffd87438c35af742b6d8db99e2327
Elmira <89019222+NalbandyanElmira@users.noreply.github.com>
2023-10-12 20:33:51 +0400

    Update trader.cpp

--- src/trader.cpp
+++ src/trader.cpp
@@ -5,13 +5,13 @@
 using namespace StockTradingSystem;
 
 TraderException::TraderException(const std::string& mess) 
-    : message(mess) 
+    	: message(mess) 
 {
 }
 
 const char* TraderException::what() const noexcept
 { 
-    return message.c_str(); 
+    	return message.c_str(); 
 }
 
 int Trader::id_counter = 0;
@@ -25,77 +25,77 @@ Trader::Trader()
 }
 
 Trader::Trader(const std::string& n)
-    : name{n}
-    , orderBook{nullptr}
-    , orderFactory{nullptr}
-    , id{id_counter++}
+    	: name{n}
+    	, orderBook{nullptr}
+    	, orderFactory{nullptr}
+    	, id{id_counter++}
 {
 }
 
 Trader::Trader(const std::string& n, const std::vector<Stock>& s, OrderFactory* o, OrderBook* b)
-    : id{id_counter++}
-    , name{n}
-    , stocks{s}
-    , orderFactory{o}
-    , orderBook{b}
+    	: id{id_counter++}
+    	, name{n}
+    	, stocks{s}
+    	, orderFactory{o}
+    	, orderBook{b}
 {
 }
 
 void Trader::buy(const StockTradingSystem::Stock& stock, int quantity)
 {
-    try
-    {
-        if (orderFactory != nullptr && orderBook != nullptr) {
-            std::shared_ptr<Order> ord = orderFactory->createOrder(id, stock.getPrice(), quantity);
-            orders.push_back(ord);
-            if (ord != nullptr) {
-                orderBook->addOrder(ord);
-            }
-        }
-    }
-    catch (const TraderException& e) {
-        // Handle trader-specific exceptions
-        std::cerr << "Trader Exception: " << e.what() << std::endl;
-    } catch (const std::exception& e) {
-        // Handle other exceptions (if any)
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
+    	try
+    	{
+        	if (orderFactory != nullptr && orderBook != nullptr) {
+            		std::shared_ptr<Order> ord = orderFactory->createOrder(id, stock.getPrice(), quantity);
+            		orders.push_back(ord);
+            		if (ord != nullptr) {
+                		orderBook->addOrder(ord);
+            		}
+        	}
+    	}
+    	catch (const TraderException& e) {
+        	// Handle trader-specific exceptions
+        	std::cerr << "Trader Exception: " << e.what() << std::endl;
+    	} catch (const std::exception& e) {
+        	// Handle other exceptions (if any)
+        	std::cerr << "Exception: " << e.what() << std::endl;
+    	}
 }
 
 void Trader::sell(const StockTradingSystem::Stock& stock, int quantity)
 {
-    try
-    {
-        if (orderFactory != nullptr && orderBook != nullptr) {
-            std::shared_ptr<Order> ord = orderFactory->createOrder(id, -stock.getPrice(), quantity);
-            orders.push_back(ord);
-            if (ord != nullptr) {
-                orderBook->addOrder(ord);
-            }
-        } 
-    }
-    catch (const TraderException& e) {
-        // Handle trader-specific exceptions
-        std::cerr << "Trader Exception: " << e.what() << std::endl;
-    } catch (const std::exception& e) {
-        // Handle other exceptions (if any)
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
+    	try
+    	{
+        	if (orderFactory != nullptr && orderBook != nullptr) {
+            		std::shared_ptr<Order> ord = orderFactory->createOrder(id, -stock.getPrice(), quantity);
+            		orders.push_back(ord);
+            		if (ord != nullptr) {
+                		orderBook->addOrder(ord);
+            		}
+        	} 
+    	}
+    	catch (const TraderException& e) {
+        	// Handle trader-specific exceptions
+        	std::cerr << "Trader Exception: " << e.what() << std::endl;
+    	} catch (const std::exception& e) {
+        	// Handle other exceptions (if any)
+        	std::cerr << "Exception: " << e.what() << std::endl;
+    	}
 }
 
 void Trader::trade(double buyThreshold, double sellThreshold, int buyQuantity, int sellQuantity)
 {
 	// Buy when the stock price is lower than a threshold and sell when it's higher
     
-    for (const Stock& stock : stocks) {
-        if (stock.getPrice() < buyThreshold) {
-            // Buy the stock with a fixed quantity (e.g., 100 shares)
-            buy(stock, buyQuantity);
-        } else if (stock.getPrice() > sellThreshold) {
-            // Sell the stock with a fixed quantity (e.g., 50 shares)
-            sell(stock, sellQuantity);
-        }
-    }
+    	for (const Stock& stock : stocks) {
+        	if (stock.getPrice() < buyThreshold) {
+            		// Buy the stock with a fixed quantity (e.g., 100 shares)
+            		buy(stock, buyQuantity);
+        	} else if (stock.getPrice() > sellThreshold) {
+            		// Sell the stock with a fixed quantity (e.g., 50 shares)
+            		sell(stock, sellQuantity);
+        	}
+    	}
 }
 
 int Trader::getID() const
@@ -104,15 +104,15 @@ int Trader::getID() const
 }
 
 const std::vector<std::shared_ptr<Order>>& Trader::getOrders() const {
-    return orders;
+    	return orders;
 }
 
 void Trader::addStock(const Stock& st)
 {
-    stocks.push_back(st);
+    	stocks.push_back(st);
 }
 
 std::vector<Stock> Trader::getStocks() const
 {
-    return stocks;
+    	return stocks;
 }
